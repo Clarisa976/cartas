aa908aef54caa290dddfa80ca8515f7925d66890
clara <calvara976@g.educaand.es>
2023-12-02 19:20:01 +0100

    le pedimos una carta al usuario, sacamos y mostramos cartas de la baraja hasta que coincida. se le ha añadido un método para pedir por teclado

--- Cartas/src/main/java/daw/Cartas.java
+++ Cartas/src/main/java/daw/Cartas.java
@@ -3,12 +3,17 @@
  */
 package daw;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author clara
  */
 public class Cartas {
-    
+
+    public static Scanner scn = new Scanner(System.in);
+
     public static void main(String[] args) {
         //creamos una carta aleatoria y la mostramos
         Naipe n1 = new Naipe();
@@ -18,5 +23,72 @@ public class Cartas {
         Baraja b1 = new Baraja();
         System.out.println(b1);
         System.out.println("\t ***********************************");
+        //pedimos una carta al usuario
+        System.out.println("Introduce el número de la carta (1-10)");
+        int numeroPedido = pedirInt();
+        //controlamos que el palo sea uno de los que hay
+        Palo paloTmp = null;
+        String paloPedido;
+        do {
+            System.out.println("Introduce el palo (OROS, BASTOS, COPAS o  ESPADAS)");
+            paloPedido = pedirString();
+            try {
+                paloTmp = Palo.valueOf(paloPedido.toUpperCase());
+                System.out.println("El usuario elige: " + numeroPedido + " "
+                        + paloPedido);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Palo no válido");
+            }
+        } while (paloTmp == null);
+
+        //sacamos y mostramos cartas hasta que coincida con la del usuario
+        boolean esIgual = false;
+        while (!esIgual) {
+            Naipe aux = b1.getBarajaNaipes().remove(0);
+            System.out.println(aux);
+            if (aux.getNumeroCartas() == numeroPedido && aux.getPalo() == paloTmp) {
+                esIgual = true;
+                System.out.println("Carta encontrada!");
+            }
+        }
+    }
+    //Método el cual controla la excepción con int
+
+    private static int pedirInt() {
+        Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+        int numero = 0;
+        do {
+            // Bloque try. Aquí se pone el código susceptible de error
+            try {
+                numero = teclado.nextInt();
+                seguir = false;
+
+            } catch (InputMismatchException ime) {
+
+                System.out.println("Introduce solo números");
+                teclado.nextLine();
+            }
+        } while (seguir);
+        return numero;
+    }
+
+    //Método el cual controla la excepción con int
+    private static String pedirString() {
+        Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+        String palabra = "";
+        do {
+            // Bloque try. Aquí se pone el código susceptible de error
+            try {
+                palabra = teclado.nextLine();
+                seguir = false;
+
+            } catch (NullPointerException npe) {
+
+                teclado.nextLine();
+            }
+        } while (seguir);
+        return palabra;
     }
 }
