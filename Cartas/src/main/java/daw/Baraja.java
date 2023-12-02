020cfa8b736483d568a599e991e3ec02e7008375
clara <calvara976@g.educaand.es>
2023-12-02 18:43:39 +0100

    añadimos los atributos, getter, setter, toString y el constructor por defecto que genera una baraja de naipes. Se le ha puesto un hashset para que no se repitan cartas

--- Cartas/src/main/java/daw/Baraja.java
+++ Cartas/src/main/java/daw/Baraja.java
@@ -4,10 +4,59 @@
  */
 package daw;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author clara
  */
 public class Baraja {
+
+    //atributo
+    private ArrayList<Naipe> barajaNaipes;
     
+    //constructor por defecto que rellena el array con todos los naipes
+    public Baraja() {
+        this.barajaNaipes = new ArrayList<>();
+        //rellenamos usando un for each
+        for (Palo palo : Palo.values()) {
+            //usamos hashset para asegurarnos de que no hay repetidos
+            Set<Integer> numUsado = new HashSet<>();
+            //se hace 10 veces el bucle porque hay 10 cartas por cada palo
+            for (int i = 0; i < 10; i++) {
+                //se genera el número de forma aleatoria
+                int numero;
+                do {
+                    numero = (int) (Math.random() * 10) + 1;
+                } while (numUsado.contains(numero));
+                //se añade a la lista
+                numUsado.add(numero);
+                try {
+                    //creamos un nuevo naipe y lo agregamos a la lista de naipes
+                    barajaNaipes.add(new Naipe(numero, palo));
+                } catch (IllegalArgumentException iae) {
+
+                }
+            }
+        }
+    }
+    //getter y setter
+    public ArrayList<Naipe> getBarajaNaipes() {
+        return barajaNaipes;
+    }
+
+    public void setBarajaNaipes(ArrayList<Naipe> barajaNaipes) {
+        this.barajaNaipes = barajaNaipes;
+    }
+    //toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Baraja{");
+        sb.append(barajaNaipes);
+        sb.append('}');
+        return sb.toString();
+    }
 }
