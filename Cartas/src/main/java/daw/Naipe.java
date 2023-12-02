f01c4b3e266d1447caf605f98b428328ab439e0e
clara <calvara976@g.educaand.es>
2023-12-02 17:48:17 +0100

    se modifica la clase naipe para añadir los atributos, el constructor, el getter, el setter y el toString

--- Cartas/src/main/java/daw/Naipe.java
+++ Cartas/src/main/java/daw/Naipe.java
@@ -9,5 +9,45 @@ package daw;
  * @author clara
  */
 public class Naipe {
+    //atributos
+    private int numeroCartas;/*1..7,8(sota),9(caballo),10(rey)*/
+    private Palo palo;//espadas, copas, oros, bastos
+    
+    //constructor aleatorio por defecto
+    public Naipe() {
+        this.numeroCartas = numeroCartas;
+        this.palo = palo;
+    }
+    /*constructor con parámetros que lanza excepción en caso de error*/
+    public Naipe(int numeroCartas, Palo palo) {
+        this.numeroCartas = numeroCartas;
+        this.palo = palo;
+    }
+    //getters
+    public int getNumeroCartas() {
+        return numeroCartas;
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
+    //setters
+    public void setNumeroCartas(int numeroCartas) {
+        this.numeroCartas = numeroCartas;
+    }
+
+    public void setPalo(Palo palo) {
+        this.palo = palo;
+    }
+    //toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Naipe{");
+        sb.append("numeroCartas=").append(numeroCartas);
+        sb.append(", palo=").append(palo);
+        sb.append('}');
+        return sb.toString();
+    }
     
 }
