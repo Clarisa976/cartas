3c03f042dc039fd24ff8207fa58b52c93728f048
clara <calvara976@g.educaand.es>
2023-12-02 18:54:09 +0100

    creamos una baraja y la mostramos

--- Cartas/src/main/java/daw/Cartas.java
+++ Cartas/src/main/java/daw/Cartas.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw;
 
 /**
@@ -9,10 +8,15 @@ package daw;
  * @author clara
  */
 public class Cartas {
-
+    
     public static void main(String[] args) {
         //creamos una carta aleatoria y la mostramos
         Naipe n1 = new Naipe();
         System.out.println("Carta creada de forma aleatoria: " + n1);
+        System.out.println("\t ***********************************");
+        //creamos una baraja y la mostramos
+        Baraja b1 = new Baraja();
+        System.out.println(b1);
+        System.out.println("\t ***********************************");
     }
 }
